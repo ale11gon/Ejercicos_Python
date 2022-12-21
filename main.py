c3ffbc3410a8dcdd35ba3850fcb6bd0c0c92824d
ale11gon <acto.gm@gmail.com>
2022-12-21 18:55:10 +0100

    ejercicio final, con boton de reinicio

---
+++ main.py
@@ -0,0 +1,29 @@
+import tkinter
+from tkinter import ttk
+
+window = tkinter.Tk()
+
+def Reiniciar(event):
+    selected.set(None)
+
+window.columnconfigure(0,weight=1)
+window.columnconfigure(1,weight=3)
+
+selected = tkinter.StringVar()
+selected.set(None)
+
+r1 = ttk.Radiobutton(window, text= "Opción 1", value="1", variable = selected)
+r2 = ttk.Radiobutton(window, text= "Opción 2", value="2", variable = selected)
+r3 = ttk.Radiobutton(window, text= "Opción 3", value="3", variable = selected)
+
+r1.grid(column=0, row=0, padx=5, pady=5)
+r2.grid(column=0, row=1, padx=5, pady=5)
+r3.grid(column=0, row=2, padx=5, pady=5)
+
+botonReiniciar = tkinter.Button(window, text='Reiniciar')
+botonReiniciar.grid(column=2, row=0, padx=5, pady=5)
+
+botonReiniciar.bind('<Button-1>', Reiniciar)
+
+
+window.mainloop()
